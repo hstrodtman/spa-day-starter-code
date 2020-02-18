ba484dd8acf7b48e6a606daf5cecae1654aa40b8
hstrodtman <heatherstrodtman@gmail.com>
2020-02-17 19:47:35 -0600

    Added validation to form submission.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message= "Please enter a username between 5 & 15 chracters.")
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password must be at least 6 characters.")
+    @Min(6)
     private String password;
+
 
     public User() {
 
